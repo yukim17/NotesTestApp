e99d001f0dd0b120be3ec4854a38cfd0faf61ee0
Kate G <kath.grish@gmail.com>
2019-01-29 21:48:06 +0300

    Initial Commit

---
+++ NotesTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesTestApp
+//
+//  Created by Екатерина on 29.01.2019.
+//  Copyright © 2019 Wineapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
